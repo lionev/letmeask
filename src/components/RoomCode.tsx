562f3d6032e8473404bc524968b5fc16670d6e08
Julio <julionev10@gmail.com>
2021-11-15 17:57:02 -0300

    Four Commit

--- src/components/RoomCode.tsx
+++ src/components/RoomCode.tsx
@@ -2,13 +2,22 @@ import copyImg from '../assets/images/copy.svg'
 
 import '../styles/room-code.scss';
 
-export function RoomCode(){
+ type RoomCodeProps = {
+  code: string;
+ }
+
+export function RoomCode(props: RoomCodeProps){
+
+  function copyRoomCodeToClipboard(){
+    navigator.clipboard.writeText(props.code)
+  }
+
   return(
-    <button className="room-code">
+    <button className="room-code" onClick={copyRoomCodeToClipboard}>
       <div>
         <img src={copyImg} alt="Copy room code" />
       </div>
-      <span>Sala #12736123</span>
+      <span>Sala {props.code}</span>
     </button>
   )
 }
